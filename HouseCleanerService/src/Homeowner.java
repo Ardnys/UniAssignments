d7c004a3be824b88b9d2d6785142a78f5e64a2c5
Ardnys <semresavas35@gmail.com>
2023-05-23 13:24:11 +0300

    neeeveeerrrmiiinddd

--- HouseCleanerService/src/Homeowner.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public class Homeowner {
-    private Person person;
-    private List<Cleaner> listOfPreviousCleaners;
-    private List<Review> reviews;
-}
--- HouseCleanerService/src/Stars.java
+++
@@ -1,8 +0,0 @@
-public enum Stars { // maybe this is good
-    ZERO,
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE
-}
