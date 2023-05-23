656a1925edd5e0c810461998b5e2bb3bccc34554
Ardnys <semresavas35@gmail.com>
2023-05-23 13:22:09 +0300

    nevermiiiind

--- 4thSemester/HouseCleanerService/src/Review.java
+++
@@ -1,7 +0,0 @@
-public class Review {
-    private Homeowner author;
-    private String title;
-    private String body;
-    // Stars enum somehow works with this
-    // make this a factory thing perhaps more readable
-}
